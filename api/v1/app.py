fb12205973d44855aa32ab8edfe647bb9aaf9d12
bernadettestella <bernadettestella27@gmail.com>
2023-10-30 16:52:40 -0700

    Restful Api

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,10 +3,11 @@
 script declares method for our app variable and port and host
 first endpoint (route) will be to return the status of your API
 """
-import os
-from flask import Flask
+from flask import Flask, make_response, jsonify
+from flask_cors import CORS
 from models import storage
 from app.v1.views import app_views
+from os import getenv
 
 # creating a variable app an instance of Flask
 app = Flask(__name__)
@@ -18,6 +19,14 @@ app.register_blueprint(app_view, url_prefix="/api/v1")
 @app.teardown_appcontext
 def close(cntxt):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    '''
+    return JSON formatted 404 status code response
+    '''
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if os.getenv("HBNB_API_HOST"):
